cc1f01564d2b5c86390d4fb584446ce7f708d572
Eldenbraz <noxuszero@gmail.com>
2025-05-03 21:11:10 +0900

    Template laby
    template laby complète

--- Scene/Days/Day3/teleporter.gd
+++ Scene/Days/Day3/teleporter.gd
@@ -4,4 +4,4 @@ extends CollisionShape3D
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	var player = get_tree().get_first_node_in_group("player")
-	player.position = Vector3(MarkerTarget.position.x, player.position.y, MarkerTarget.position.z)
+	player.position = Vector3(MarkerTarget.position.x, MarkerTarget.position.y, MarkerTarget.position.z)
